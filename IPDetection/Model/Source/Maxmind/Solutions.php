ba79679303e7609d3619164fc02a24b2a5103dd5
Trung Luu <luuvantrung@gmail.com>
2021-12-13 16:20:25 +0700

    Update Solutions.php

--- IPDetection/Model/Source/Maxmind/Solutions.php
+++ IPDetection/Model/Source/Maxmind/Solutions.php
@@ -3,14 +3,23 @@ namespace Betagento\IPDetection\Model\Source\Maxmind;
 
 class Solutions {
     
+    /**
+     * @var array<string,string>
+     */
+    protected $solutions;
+    
+    /**
+     * @param array<string,string> $solutions
+     */
     public function __construct(array $solutions = array() ) {
         $this->solutions = $solutions;
     }
 
-
+    /**
+     * @return array<mixed>
+     */
     public function toOptionArray()
     {
-       
         $options = array(); 
         foreach($this->solutions as $key => $value){
             $options[] = [
